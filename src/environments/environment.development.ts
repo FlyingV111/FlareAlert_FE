577626973e00b831bbe9eca5564ff9bb9bfcc5af
FlyingV111 <paul.nassauer@gmail.com>
2025-01-06 22:06:50 +0100

    add environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  baseUrl: "http://localhost:8042",
+  backendUrl: "http://localhost:8042/api/v1",
+  backendAuthUrl: "http://localhost:8042/auth",
+  firebaseConfig : {
+    apiKey: "AIzaSyAaKDcdHgfyUefxviw4ds7vvt2q6QSKGY8",
+    authDomain: "flarealert-fd206.firebaseapp.com",
+    projectId: "flarealert-fd206",
+    storageBucket: "flarealert-fd206.firebasestorage.app",
+    messagingSenderId: "222907678065",
+    appId: "1:222907678065:web:88bf10eab44b0ae3518ad0",
+    measurementId: "G-WNDW3VLZ2H"
+  }
+};
